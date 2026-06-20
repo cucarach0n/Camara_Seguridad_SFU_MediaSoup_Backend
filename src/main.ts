a2ed25f6c28dd7226b7e9a6f697cb567b0a49636
cucarach0n <luis_ls11@hotmail.com>
2026-06-20 15:05:21 -0500

    se cambia el puerto en el .env

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
-  await app.listen(3000);
-  console.log('Backend Mediasoup is running on http://localhost:3000');
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Backend Mediasoup is running on port ${port}`);
 }
 bootstrap();
